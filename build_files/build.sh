d718429f18412e889a0518acc1cd9b49c2c93a55
approx99balloons <approx99balloons@gmail.com>
2025-04-21 13:00:06 +0300

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
-
+ 
 set -ouex pipefail
-
+ 
 ### Install packages
-
+ 
 # Packages can be installed from any enabled yum repo on the image.
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
+ 
+mkdir /vault
+chown 1000:1000 /vault
+ 
 # this installs a package from fedora repos
 dnf5 install -y tmux 
-
+dnf5 install -y dnf-plugins-core
+dnf-3 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+ 
+dnf5 install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+ 
+dnf5 install -y htop
+dnf5 install -y screenfetch
+ 
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
 # dnf5 -y install package
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
-
+ 
 #### Example for enabling a System Unit File
-
-systemctl enable podman.socket
+ 
+systemctl enable docker.socket
